6bbc5843f5cd9fad1999a695dace7f4116bcfbfa
Kiran Karra <kiran.karra@gmail.com>
2023-10-08 09:22:05 -0400

    updated readme

--- universal_multipliers_web/SP_Cost_Explorer.py
+++ universal_multipliers_web/SP_Cost_Explorer.py
@@ -12,7 +12,27 @@ st.sidebar.success("Select a Page above.")
 
 st.markdown(
     """
-    ### Universal Multipliers Incentives Exploration
+    ### Universal Multipliers
+This application explores the effect of universal multipliers on the Filecoin economy. Specifically, the calculator computes the difference in Fiat ROI between CC miners and variants of Fil+ miners.
+`FiatROI = NetIncome[Fil+ Variant] - NetIncome[CC]`. 
+
+Please refer to the associated [report]() for more details and to better understand the context.
+
+### Cost Computation
+In the charts for both calculators, `net_income = revenue - costs`. The following table outlines all of the revenue and cost sources.  While most cost sources are adjustable via slider bar widgets, some are fixed due to their negligible impact on the overall cost.
+
+Note that all revenue and costs are in units of $/TiB/Yr. 
+
+|Revenue ($/TiB/Yr) |Fixed Costs ($/TiB/Yr)| Adjustable Costs ($/TiB/Yr)
+|--|--|--|
+|Block Rewards  |Sealing ($1.30) | Power Cost
+|Deal Revenue  | Gas Cost w/ PSD ($2.30) | Bandwidth Cost
+| | Gas Cost w/out PSD ($0.10) | Staff Cost
+| | | Pledge (% of Block Rewards)
+| | | Data Prep
+| | | FIL+ BizDev
+| | | RD BizDev
+| | | Storing Extra Copy
 
 
 ### How to use this app
